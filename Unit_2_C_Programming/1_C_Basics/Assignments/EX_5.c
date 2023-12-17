c289dbc531475ade1e55f0b871f280b4e2cc711c
Moataz-Elhawary <moataz.elhawary311@gmail.com>
2023-12-17 22:28:04 +0200

    Update

---
+++ Unit_2_C_Programming/1_C_Basics/Assignments/EX_5.c
@@ -0,0 +1,20 @@
+/*
+ ======================================================================================================================
+ Name           : EX_5.c
+ Author         : Moataz Elhawary
+ Created on	    : Dec 17, 2023
+ ======================================================================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char chr ;
+    printf("Enter a character : \n");
+    scanf("%c",&chr);
+
+    printf("\r\nASCII value of %c : %d \r\n",chr,chr);
+    return 0;
+}
