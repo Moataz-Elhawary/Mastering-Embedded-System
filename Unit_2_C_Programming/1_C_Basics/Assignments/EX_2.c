9cf55ed6cbb17d72f4e81a19009678f4c84dbace
Moataz-Elhawary <moataz.elhawary311@gmail.com>
2023-12-17 21:18:58 +0200

    Update

---
+++ Unit_2_C_Programming/1_C_Basics/Assignments/EX_2.c
@@ -0,0 +1,20 @@
+/*
+ ======================================================================================================================
+ Name        : EX_2.c
+ Author      : Moataz Elhawary
+ Created on	 : Des 17, 2023
+ ======================================================================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int integer_number=0 ;
+    printf("Enter a integer Number : ");
+    scanf("%d",&integer_number);
+
+    printf("\r\nYou Entered : %d\r\n",integer_number);
+    return 0;
+}
